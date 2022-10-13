b1a2fd39ef5502b9ce70a27192adeca2ade8c243
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-10-13 14:21:17 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -12,3 +12,32 @@ for(i=0;i <200 ; i++){
 }
 //=====================  FIZZ BUZZ ====================
 for(i=1;i <100 ; i++)
+if(i%3 === 0 && i%5 ===0){
+    console.log('fizzBuzz')
+}
+else if (i % 5 === 0){
+    console.log('buzz')
+}
+else if( i% 3 === 0){
+console.log('Fizz')
+}
+//===================== Wild Wild Life ==================
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee[2]=50001
+console.log(plantee)
+wolfy[3]="Gotham City"
+
+dart.push("Hawkins")
+console.log(dart)
+wolfy[0]="Gameboy"
+console.log(wolfy)
+//==================== Yell at the Ninja Turtles ==============
+const ninjaTurtles=[Donatello, Leonardo, Raphael, Michaelangelo]
+for(i=0; i<ninjaTurtles.length;i++){
+ninjaTurtles[i].toUpperCase()
+}
+console.log(ninjaTurtles)
